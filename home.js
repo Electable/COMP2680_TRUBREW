488c79a706c5d54b7f6c76b737e4f8a477d315a7
jcjahn <jacobjahn@outlook.com>
2025-11-30 15:25:49 -0800

    Delete home.js

--- home.js
+++
@@ -1,7 +0,0 @@
-lastMod();
-setInterval(() => lastMod(), 1000);
-//return last modified
-function lastMod(){
-    let modDisplay = document.getElementById("lastMod");
-    modDisplay.innerText = "\u00A9 2025 TruBrew | Last Modified: " + document.lastModified;
-}
