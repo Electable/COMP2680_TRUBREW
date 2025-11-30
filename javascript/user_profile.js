c0bd751523bbc8f32bdd67717bf54b84e76d3c1d
jcjahn <jacobjahn@outlook.com>
2025-11-30 15:29:12 -0800

    Add files via upload

--- javascript/user_profile.js
+++ javascript/user_profile.js
@@ -20,3 +20,14 @@ function lastMod(){
     let modDisplay = document.getElementById("lastMod");
     modDisplay.innerText = "\u00A9 2025 TruBrew | Last Modified: " + document.lastModified;
 }
+
+var ccn = document.getElementById('ccn');
+var submitButton = document.querySelector('button[type="submit"]');
+
+submitButton.addEventListener('click', function(e){
+    e.preventDefault();
+
+    const number = ccn.value;
+    localStorage.setItem('creditCardNumber', number);
+    window.location.href = 'order.html';
+})
